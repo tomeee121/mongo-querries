f00ce8cbc3de5e64633e5d25102f4ff274f66783
Tomasz Borowski <bortom112233@gmail.com>
2023-02-15 18:44:18 +0100

    Criteria api querry

--- src/main/java/TB/mongoquerries/config/SwaggerConfig.java
+++ src/main/java/TB/mongoquerries/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package TB.mongoquerries.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -15,7 +16,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("TB.mongoquerries.controller"))
+                .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
     }
--- src/main/java/TB/mongoquerries/controller/EmployeeController.java
+++ src/main/java/TB/mongoquerries/controller/EmployeeController.java
@@ -3,15 +3,21 @@ package TB.mongoquerries.controller;
 import TB.mongoquerries.model.Employee;
 import TB.mongoquerries.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RestController
+@RequestMapping("/api")
 public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
     @PostMapping("/employees")
     List<Employee> saveAll(@RequestBody List<Employee> employees) {
@@ -21,6 +27,13 @@ public class EmployeeController {
     @GetMapping("/employees")
     List<Employee> findAll() {
         return employeeRepository.findAll();
+    }
+
+    @GetMapping("/employees/{name}")
+    List<Employee> findByName(@PathVariable(value = "name") String name) {
+        Query query = new Query();
+        query.addCriteria(Criteria.where("name").is(name));
+        return mongoTemplate.find(query, Employee.class);
     }
 
 
