fa2f7c4df7d6a7411a2d97f63724f8907595cd7b
Tomasz Borowski <bortom112233@gmail.com>
2023-02-15 19:42:17 +0100

    JPQL querry with defining document fields to hide

--- src/main/java/TB/mongoquerries/repository/EmployeeRepository.java
+++ src/main/java/TB/mongoquerries/repository/EmployeeRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @Repository
 public interface EmployeeRepository extends MongoRepository<Employees, String> {
-    @Query("{'name': ?0}")
+    @Query(value = "{'name': ?0}", fields = "{'id':0, 'firstname':0}")
     List<Employees> findByName(String name);
 
     @Query("{'name': ?0, 'department': ?1}")
